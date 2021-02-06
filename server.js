c02a38f3f694aa10400dfd5240aaebba6ae726b7
Oscar Hernandez <oscarjavierho@gmail.com>
2021-02-06 16:10:51 -0430

    Changes test

--- server.js
+++ server.js
@@ -1,18 +1,21 @@
 const express = require('express');
 const app =express();
 
-let PORT = 5000;
-console.log("NODE_ENV is", process.env.NODE_ENV);
-app.listen(PORT, () => console.log("Server is runing on port", PORT));
+let port;
+if (process.env.NODE_ENV === "production") {
+  port = process.env.PORT || 3000;
+  app.use(express.static(path.join(__dirname, "../build")));
+  app.get("*", (request, response) => {
+    response.sendFile(path.join(__dirname, "../build", "index.html"));
+  });
+} else {
+  port = 3001;
+}
 
-// Serve static assets in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+app.listen(port, () => {
+  console.log("Express server is runing on port", port);
+  console.log("NODE_ENV is", process.env.NODE_ENV);
+});
 
 app.get('/api', (req, res) => {
     res.json({msg: "Api Runing"})
